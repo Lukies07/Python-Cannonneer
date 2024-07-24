54dcda65ababb3e4d0debd7a912ef08981a4ff64
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2024-07-25 10:59:37 +1200

    saving changes

--- Main.py
+++ Main.py
@@ -4,20 +4,40 @@ pygame.init()
 
 # Get screen dimensions
 screen_info = pygame.display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
+screen_width = screen_info.current_w*0.95
+screen_height = screen_info.current_h*0.95
+
 
 # Set up the display
-screen = pygame.display.set_mode((screen_width*0.95, screen_height*0.95))
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#get centre of canvas
+canvas_centre_x = screen_width/2
+canvas_centre_y = screen_height/2
+
+lvl_boxes = [
+    pygame.Rect(50, 50, 100, 100),
+    pygame.Rect(200, 50, 100, 100),
+    pygame.Rect(350, 50, 100, 100),
+    pygame.Rect(500, 50, 100, 100),
+    pygame.Rect(650, 50, 100, 100),
+]
+
 
 # Main loop
-running = True #I set it to true 
+running = True
 while running:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+        elif event.type == pygame.MOUSEMOTION: #obtain the coords
+            mouse_pos = pygame.mouse.get_pos()
+            print(f"Mouse coordinates: {mouse_pos}")
+
+    screen.fill((173, 165, 160)) #set the bacground to grey
+    pygame.draw.rect(screen, (255, 0, 0), lvl_boxes)
 
     # Update the display
     pygame.display.flip()
